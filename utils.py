622f75dcce38ad097e4040b4eba6f2bd2ba4fd3c
prvnsingh <prvns1997@gmail.com>
2022-11-23 21:30:42 -0500

    adding metrics and requirements

--- utils.py
+++ utils.py
@@ -130,3 +130,37 @@ def print_metrics(gt_image, noisy_image, guass_output, nl_output, noise_type="Ga
     print(f"MSE error for noisy image = {mean_squared_error(gt_image, noisy_image)}")
     print(f"MSE error after gaussian filter = {mean_squared_error(gt_image, guass_output)}")
     print(f"MSE error for predicted image = {mean_squared_error(gt_image, nl_output)}")
+
+
+def MSE(image1, image2):
+    """
+    Mean Squared Error
+    :param image1: image1
+    :param image2: image2
+    :rtype: float
+    :return: MSE value
+    """
+
+    # Calculating the Mean Squared Error
+    mse = np.mean(np.square(image1.astype(np.float) - image2.astype(np.float)))
+
+    return mse
+
+
+def PSNR(image1, image2, peak=255):
+    """
+    Peak signal-to-noise ratio
+    :param image1: image1
+    :param image2: image2
+    :param peak: max value of pixel 8-bit image (255)
+    :rtype: float
+    :return: PSNR value
+    """
+
+    # Calculating the Mean Squared Error
+    mse = MSE(image1, image2)
+
+    # Calculating the Peak Signal Noise Ratio
+    psnr = 10 * np.log10(peak ** 2 / mse)
+
+    return psnr
